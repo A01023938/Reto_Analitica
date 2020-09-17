7c01ecabdbb6e277019f48e088360fdcc238689c
Mateo Gonzalez Cosio <mateoglzc@hotmail.com>
2020-09-17 13:37:17 -0500

    Fixed errors ❌

--- distancia.py
+++ distancia.py
@@ -4,9 +4,9 @@ list2 = [5,4,3,2,1]
 def distance(list1, list2):
     if len(list1) != len(list2):
         return -1
-    
+    d_squared = 0
     for v1, v2 in zip(list1, list2):
-        d_squared = (v1 - v2)**2
+        d_squared += (v2 - v1)**2
 
     return d_squared**(1/2)
 
