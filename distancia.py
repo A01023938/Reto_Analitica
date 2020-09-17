c1db9eeef8c743f6dfdd477413308ec86929b3ab
Mateo Gonzalez Cosio <mateoglzc@hotmail.com>
2020-09-17 13:33:15 -0500

    Distance formula 🚶‍

---
+++ distancia.py
@@ -0,0 +1,13 @@
+list1 = [1,2,3,4,5]
+list2 = [5,4,3,2,1]
+
+def distance(list1, list2):
+    if len(list1) != len(list2):
+        return -1
+    
+    for v1, v2 in zip(list1, list2):
+        d_squared = (v1 - v2)**2
+
+    return d_squared**(1/2)
+
+print(distance(list1, list2))
